2214edbb2c5de98af8e2724936297a97c6673353
Vitali <vitalisbayer@gmail.com>
2021-11-19 11:49:13 +0300

    clear: delete all custom components

--- angular-study-project/src/app/app-routing.module.ts
+++ angular-study-project/src/app/app-routing.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ContactCardsComponent } from './contact-cards/contact-cards.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
-import { MainLayoutComponent } from './main-layout/main-layout.component';
 
-const routes: Routes = [
-  {path: '', component: MainLayoutComponent,
-  children: [
-    {path: '', redirectTo: '/', pathMatch: 'full'},
-    {path: '', component: ContactListComponent},
-    {path: 'cards', component: ContactCardsComponent},
-    {path: 'about', component: AboutComponent}
-  ]}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-study-project/src/app/app.module.ts
+++ angular-study-project/src/app/app.module.ts
@@ -6,18 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 
-import { MainLayoutComponent } from './main-layout/main-layout.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
-import { ContactCardsComponent } from './contact-cards/contact-cards.component';
-import { AboutComponent } from './about/about.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    MainLayoutComponent,
-    ContactListComponent,
-    ContactCardsComponent,
-    AboutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
