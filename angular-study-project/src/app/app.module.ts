d10921e6fd40f5007050d8da7e865f0ae90d4b51
Vitali <vitalisbayer@gmail.com>
2021-12-20 13:02:04 +0300

    fix: add losed layout module to app module

--- angular-study-project/src/app/app.module.ts
+++ angular-study-project/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
 
 
 
@@ -16,7 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
